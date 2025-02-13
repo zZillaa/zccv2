818d772e799fe073d1a86e204d0526bf15b832ed
Azeem <iamazeemmuhammad@gmail.com>
2025-02-12 18:14:57 -0800

    Fixing directives

--- prac.c
+++ prac.c
@@ -1,10 +1,8 @@
 int arr[5] = {1, 2, 3, 4, 5};
 int z[3] = {0, 0, 0};
 int s[2] = {2, 1};
+int other[5] = {6, 7, 8, 9, 10};
 
-int function() {
-	int x = 3;
-	int z = 2 + x;
-
-	return z;
+int add(int a, int b) {
+	return a + b;
 }
